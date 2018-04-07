1bfc7494a76527ccc0822d4cbfe961ab2be17783
jennifersalas <jennysalas@me.com>
2018-04-06 21:27:43 -0700

    Implemented 3d using matplot lib with plans to switch over to OpenGL

--- animation.py
+++ animation.py
@@ -15,10 +15,11 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+import numpy
 import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.animation as animation
 
-import numpy
 
 import standardFuncs
 import defaultValues
@@ -37,18 +38,28 @@ def showPaths(planes):
     uLon = agrid[1][1]
 
     fig = plt.figure()
-    ax = plt.axes(xlim=(lLat, uLat), ylim=(lLon, uLon))
+    ax = p3.Axes3D(fig)
 
     planePlots = [plt.plot([], [], "bo")[0] for _ in range(len(planes))]
 
     aLat = int((uLat - lLat) * standardFuncs.LATITUDE_TO_METERS) // 2
     aLon = int((uLon - lLon) * standardFuncs.LONGITUDE_TO_METERS) // 2
-    plt.xticks([lLat, uLat], ["West\n%im" % -aLat, "East\n%im" % aLat])
-    plt.yticks([lLon, uLon], ["South\n%im\n" % -aLon, "North\n%im\n" % aLon],
-               rotation=90)
-    plt.xlabel("Three Dimensional Collision Avoidance")
 
-    fig.subplots_adjust(bottom=.15)
+    # plt.xticks([lLat, uLat], ["West\n%im" % -aLat, "East\n%im" % aLat])
+    ax.set_xlim3d([lLat, uLat])
+    ax.set_xlabel('Latitude')
+
+    # plt.yticks([lLon, uLon], ["South\n%im\n" % -aLon, "North\n%im\n" % aLon],
+    #            rotation=90)
+    ax.set_ylim3d([lLon, uLon])
+    ax.set_ylabel('Longitude')
+
+    ax.set_zlim3d([0, 20])
+    ax.set_zlabel('Altitude')
+
+    ax.set_title("Three Dimensional Collision Avoidance")
+
+    # fig.subplots_adjust(bottom=.15)
 
     def init():
         for planeP in planePlots:
@@ -62,11 +73,12 @@ def showPaths(planes):
 
     def animate(i):
         for j,planePlot in enumerate(planePlots):
-            planePlot.set_markerfacecolor('k' if i[j]["dead"] else 'b')
             planePlot.set_data(i[j]["Latitude"],i[j]["Longitude"])
-            planePlot.set_marker((3,0,i[j]["bearing"]-90))
-
+            planePlot.set_marker((3,0,i[j]["bearing"]))
+            planePlot.set_markerfacecolor('r' if i[j]["dead"] else 'y' if i[j]["wpflag"] else 'b')
+            planePlot.set_3d_properties(((i[j]["Altitude"]-375) / 50) * 20)
         return planePlots
+
     planeMat = numpy.array(planes).T
 
     print(planeMat.shape)
